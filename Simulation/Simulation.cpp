14049b8e9d1d5b78848b982e9f9143e2072bd0af
abner <apalmeira@datakitchen.io>
2022-06-16 08:39:58 -0300

    cleanup Simulation Class

--- Simulation/Simulation.cpp
+++ Simulation/Simulation.cpp
@@ -5,7 +5,6 @@ Simulation::Simulation(int width, int height){
 	height_ = height;
 	size_ = width * height;
     buffer_ = std::vector<MagicPixel*>(size_,nullptr);
-    revive_ = new int[size_];
     chunk_ = new Chunk[64];
     jobs_ = new Job[64];
     int x = 0, y = 0;
@@ -23,103 +22,61 @@ Simulation::Simulation(int width, int height){
 }
 
 void Simulation::Reset(){
-    
+    for(std::vector<MagicPixel *>::iterator it = buffer_.begin(); it != buffer_.end(); it++){
+        if(*it != nullptr) delete *it;
+    }
 }
 
 Simulation::~Simulation(){
     for(std::vector<MagicPixel *>::iterator it = buffer_.begin(); it != buffer_.end(); it++){
-        if(*it != NULL) delete *it;
+        if(*it != nullptr) delete *it;
     }
     delete[] chunk_;
 	SDL_FreeFormat(pixel_format_);
     pool_.Stop();
 }
 
-void Simulation::Red(){
+void Simulation::ProcessChunk(int *chunks){
     int done = 0;
-    int red[] = {0,4,10,14,16,20,26,30,32,36,42,46,48,52,58,62};
-    for(int i=0;i<16;i++) pool_.QueueJob(jobs_[red[i]]);
+    for(int i=0;i<16;i++) pool_.QueueJob(jobs_[chunks[i]]);
     while(done != 16){
         done = 0;
-        for(int i=0;i<16;i++) done += chunk_[red[i]].last_frame_ == frame_count;
-    }
-}
-
-void Simulation::Green(){
-    int done = 0;
-    int green[] = {1,5,11,15,17,21,27,31,33,37,43,47,49,53,59,63};
-    for(int i=0;i<16;i++) pool_.QueueJob(jobs_[green[i]]);
-    while(done != 16){
-        done = 0;
-        for(int i=0;i<16;i++) done += chunk_[green[i]].last_frame_ == frame_count;
-    }
-}
-
-void Simulation::Blue(){
-    int done = 0;
-    int blue[] = {2,6,8,12,18,22,24,28,34,38,40,44,50,54,56,60};
-    for(int i=0;i<16;i++) pool_.QueueJob(jobs_[blue[i]]);
-    while(done != 16){
-        done = 0;
-        for(int i=0;i<16;i++) done += chunk_[blue[i]].last_frame_ == frame_count;
-    }
-}
-
-void Simulation::Orange(){
-    int done = 0;
-    int orange[] = {3,7,9,13,19,23,25,29,35,39,41,45,51,55,57,61};
-    for(int i=0;i<16;i++) pool_.QueueJob(jobs_[orange[i]]);
-    while(done != 16){
-        done = 0;
-        for(int i=0;i<16;i++) done += chunk_[orange[i]].last_frame_ == frame_count;
+        for(int i=0;i<16;i++) done += chunk_[chunks[i]].last_frame_ == frame_count;
     }
 }
 
 void Simulation::Update(){
-//    typedef void (Simulation::*method_function)();
-//    method_function arr[] = {&Simulation::Red,&Simulation::Green,&Simulation::Blue,&Simulation::Orange};
-//    std::random_device rd;
-//    std::mt19937 g(rd());
-//    std::shuffle(arr, arr+4, g);
-//    for(int i=0;i<4;i++){
-//        method_function func = arr[i];
-//        (this->*func)();
-//    }
-    Red();
-    Green();
-    Blue();
-    Orange();
-
+    ProcessChunk(first_batch_);
+    ProcessChunk(second_batch_);
+    ProcessChunk(third_batch_);
+    ProcessChunk(fourth_batch_);
 }
 
-void Simulation::SetCell(Uint32 index, MaterialType material, bool physics){
+void Simulation::SetCell(SDL_Point point, MaterialType material, bool physics){
+    Uint32 index = point.x + point.y * width_;
     if(material == MaterialType::EMPTY && buffer_[index] != nullptr){
         delete buffer_[index];
         buffer_[index] = nullptr;
+        chunk_[Helper::GetChunk(index)].RemoveCell(point.x,point.y);
     }
 	else if(material != MaterialType::EMPTY && buffer_[index] == nullptr){
         buffer_[index] = Material::Init(index, material, &buffer_);
+        chunk_[Helper::GetChunk(index)].AddCell(point.x,point.y);
         if(physics){
             std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
-//            buffer_[_index].velocity_ = Vector2(std::uniform_int_distribution<int>(0, 5)(rng),std::uniform_int_distribution<int>(0, 5)(rng));
-//            buffer_[_index].has_velocity_ = true;
         }
 	}
 }
 
-//Sets a circle of cells by radius for user input
-void Simulation::SetCellRadius(SDL_Point point, Uint16 rad, MaterialType material, bool physics){
+void Simulation::SetCellInsideRadius(SDL_Point point, Uint16 rad, MaterialType material, bool physics){
     int sqr = rad * rad;
     for (int i = -rad; i <= rad; i++){
         for (int j = -rad; j <= rad; j++){
             if (i * i + j * j <= sqr){
                 Sint32 x = point.x + i;
                 Sint32 y = point.y + j;
-                Uint32 index = x + y * width_;
                 if(y >= 0 && y < height_ && x >= 0 && x < width_){
-                    SetCell(index, material,physics);
-                    if(material != MaterialType::EMPTY) chunk_[Helper::GetChunk(index)].AddCell(x,y);
-                    else chunk_[Helper::GetChunk(index)].RemoveCell(x,y);
+                    SetCell({x,y}, material,physics);
                 }
                 
             }

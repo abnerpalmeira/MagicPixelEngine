8878daebfacc0ef54826f4d15a9c1b3f4f9caf61
Abner Palmeira <abner.palmeira@gmail.com>
2022-06-16 18:22:41 -0300

    refactor(chunk): update Chunk.cpp and Chunk.hpp

--- Chunk/Chunk.hpp
+++ Chunk/Chunk.hpp
@@ -37,10 +37,12 @@ public:
     void UpdateRect(int x, int y);
     void AddCell(int x, int y);
     void RemoveCell(int x, int y);
+    void Notify(int x, int y);
+    
     void MoveCell(int from,int to);
-    void NotifyBottom();
-    void NotifyLeft();
-    void NotifyRight();
+    void NotifyBottom(int min_x,int max_x);
+    void NotifyLeft(int min_y,int max_y);
+    void NotifyRight(int min_y,int max_y);
     void NotifyBottomLeft();
     void NotifyBottomRight();
     void ResetRect();

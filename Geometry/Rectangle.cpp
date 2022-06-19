694df21ba0af0eac772d2371209a21bc979ba8f5
Abner Palmeira <abner.palmeira@gmail.com>
2022-06-19 15:10:18 -0300

    feat(geometry): add Rectangle.cpp and Rectangle.hpp

---
+++ Geometry/Rectangle.cpp
@@ -0,0 +1,8 @@
+//
+//  Rectangle.cpp
+//  MagicPixelEngine
+//
+//  Created by Abner Palmeira on 19/06/22.
+//
+
+#include "Rectangle.hpp"
---
+++ Geometry/Rectangle.hpp
@@ -0,0 +1,13 @@
+//
+//  Rectangle.hpp
+//  MagicPixelEngine
+//
+//  Created by Abner Palmeira on 19/06/22.
+//
+
+#ifndef Rectangle_hpp
+#define Rectangle_hpp
+
+#include <stdio.h>
+
+#endif /* Rectangle_hpp */

67995a30e8bb8998747e68f0db6856095f42131b
Abner Palmeira <abner.palmeira@gmail.com>
2022-06-28 10:24:49 -0300

    refactor(material): update Fire.cpp, Wood.cpp and Cell.cpp

--- Material/Custom/Wood.cpp
+++ Material/Custom/Wood.cpp
@@ -10,6 +10,8 @@
 Wood::Wood(){
     color_ = Color(106,75,63,255);
     ignite_temperature_ = 250;
+    surface_area_ = 80;
+    material_ = MaterialType::WOOD;
     if(Helper::CoinToss()) color_ = Color::Interpolate(color_, Color::Black, Helper::RandomDoubleOnInterval(0.0, 0.4));
 }
 

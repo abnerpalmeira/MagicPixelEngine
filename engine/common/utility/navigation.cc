a5470505b15e559200bb05c8e67118f8cfc5556f
Abner Palmeira <abnerpalmeira@MacBook-Pro-de-Abner-2.local>
2025-05-14 10:19:14 -0300

    Force

---
+++ engine/common/utility/navigation.cc
@@ -0,0 +1,5 @@
+// navigation.cc
+#include "common/utility/navigation.h"
+
+int Navigation::dx[8] = {0, 0, 1,-1,1,-1, 1,-1};
+int Navigation::dy[8] = {1,-1, 0, 0,1, 1,-1,-1};
--- engine/common/utility/navigation.h
+++ engine/common/utility/navigation.h
@@ -1,17 +1,8 @@
-//
-//  navigation.h
-//  MagicPixelEngine
-//
-//  Created by Abner Palmeira on 27/06/22.
-//
-
+// navigation.h
 #pragma once
 
-class Navigation{
+class Navigation {
 public:
     static int dx[8];
     static int dy[8];
 };
-
-int Navigation::dx[8] = {0, 0, 1,-1,1,-1, 1,-1};
-int Navigation::dy[8] = {1,-1, 0, 0,1, 1,-1,-1};

3f90a52a1a04053033288ea21fcab2349fe605f0
Abner Palmeira <abnerpalmeira@MacBook-Pro-de-Abner-2.local>
2025-05-14 11:37:59 -0300

    sun

--- engine/magicpixel/material/custom/rock.cc
+++ engine/magicpixel/material/custom/rock.cc
@@ -6,8 +6,37 @@
 //
 
 #include "magicpixel/material/custom/rock.h"
+#include "magicpixel/material/custom/lava.h"
+#include "common/utility/navigation.h"
 
-Rock::Rock(){
-    color_ = Color(58,50,50,255);
-    if(Random::CoinToss()) color_ = Color::Interpolate(color_, Color::White, Random::DoubleOnInterval(0.0, 0.15));
+Rock::Rock() {
+    color_ = Color(58, 50, 50, 255);
+    material_ = MaterialType::ROCK;
+    if(Random::CoinToss()) {
+        color_ = Color::Interpolate(color_, Color::White, Random::DoubleOnInterval(0.0, 0.15));
+    }
+}
+
+void Rock::Update(Buffer &buffer, int x, int y) {
+    // Check if it should melt
+    if (temperature_ >= melt_temperature) {
+        buffer.ReplacMagicPixel(MaterialType::LAVA, x, y);
+        return;
+    }
+    
+    // Heat up from nearby heat sources
+    for (int i = 0; i < 8; i++) {
+        int a = x + Navigation::dx[i], b = y + Navigation::dy[i];
+        if (a < 0 || b < 0 || a >= kSimulationWidth || b >= kSimulationHeight) continue;
+        
+        if (!buffer.IsCellEmpty(a, b)) {
+            MaterialType neighbor = buffer.GetMaterial(a, b);
+            if (neighbor == MaterialType::FIRE || neighbor == MaterialType::SUN || neighbor == MaterialType::LAVA) {
+                buffer.buffer_[a][b].TransferHeat(temperature_);
+            }
+        }
+    }
+    
+    // Call parent class Update
+    MagicPixel::Update(buffer, x, y);
 }
--- engine/magicpixel/material/custom/rock.h
+++ engine/magicpixel/material/custom/rock.h
@@ -12,10 +12,14 @@
 #include "magicpixel/material/material_type.h"
 #include "magicpixel/magic_pixel_factory.h"
 #include "magicpixel/material/base/static.h"
+#include "common/utility/navigation.h"
 
-class Rock : public Static{
+class Rock : public Static {
 public:
     Rock();
+    void Update(Buffer &buffer, int x, int y) override;
+private:
+    static constexpr Uint32 melt_temperature = 300;  // Same as Sun's MELTING_TEMPERATURE
 };
 
 namespace {
